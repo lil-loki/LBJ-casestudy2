fc25d25c1ae74f0397fdc71bd802eb01ffb14cd5
lil-loki <lokesh.sivakumar@gmail.com>
2021-08-06 12:59:44 +0530

    final commit

--- backend.py
+++ backend.py
@@ -5,13 +5,6 @@ from flask import Flask,render_template,request,Response,jsonify
 app=Flask(__name__)   
 
 
-# with open('students.csv', 'w', newline='') as csvfile:
-#     fieldnames = ['Student ID','Student Name','Gender','DateOfBirth','City','State','EmailId','Qualification','Stream']
-#     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-#     writer.writeheader()
-#     writer.writerow({'Student ID': '123','Student Name': 'lo','Gender': 'Male', 'DateOfBirth': '15/7/77','City': 'Chennai', 'State': 'TN','EmailId': 'lo@gmail.com', 'Qualification': 'BE','Stream': 'ECE'})
-
-
 @app.route("/")
 def home():
     return render_template('index.html')
@@ -32,14 +25,29 @@ def addstudent():
         with open('students.csv', 'a', newline='') as csvfile:
             fieldnames = ['Student ID','Student Name','Gender','DateOfBirth','City','State','EmailId','Qualification','Stream']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            writer.writerow({'Student ID': Student_ID,'Student Name': Student_Name,'Gender': Gender, 'DateOfBirth': DateOfBirth ,'City': City, 'State': State,'EmailId': EmailId, 'Qualification': Qualification,'Stream': Stream})
+            writer.writerow({'Student ID': Student_ID,'Student Name': Student_Name,'Gender': Gender, 
+            'DateOfBirth': DateOfBirth ,'City': City, 'State': State,'EmailId': EmailId, 'Qualification': Qualification,
+            'Stream': Stream})
         return render_template('index.html')
     else:
         return render_template('add-student.html')
 
-@app.route("/searchstudent")
+@app.route("/searchstudent",methods =["GET", "POST"])
 def searchstudent():
-    return render_template('search-student.html')
+    rows=0
+    tstate=False
+    if request.method == "POST":
+        Student_ID=request.form.get("Search")
+        print(Student_ID)
+        with open('students.csv', 'r', newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                if row['Student ID']==str(Student_ID):
+                    rows=row
+        tstate=True
+        return render_template('search-student.html',row=rows,tstate=tstate)
+    else:
+        return render_template('search-student.html',row=rows,tstate=tstate)
 
 @app.route("/displaystudents")
 def displaystudents():
